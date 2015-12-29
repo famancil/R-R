ba74d019287ef3a402aa3528392375f33f6e1247
Ecklys <sebafernandezvera@gmail.com>
2015-12-29 03:28:40 -0300

    Prototipo de Ver Estadística (principalmente listar cada actividad y ver si se ha cumplido)

--- app/src/main/java/com/example/famancil/myapplication/Estadisticas.java
+++ app/src/main/java/com/example/famancil/myapplication/Estadisticas.java
@@ -1,23 +1,60 @@
 package com.example.famancil.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 
 
 public class Estadisticas extends AppCompatActivity {
 
-    private DataBaseHelper DataBaseHelper;
-    private Usuario User;
+    DataBaseHelper dataBaseHelper;
+    Button  button_ver;
+    String Id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estadisticas);
-    }
-    public void GenerarEstadistica(View view) {
+        button_ver = (Button) findViewById(R.id.button_statistics);
 
+        Intent intent= getIntent();
+        dataBaseHelper=DataBaseHelper.getInstance(this);
+        Cursor uno = Prioridad.BuscarPrioridad(dataBaseHelper, Id);
+
+        viewData();
+    }
+    public void viewData() {
+        button_ver.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor activity = Actividad.BuscarTodosActividades(dataBaseHelper);
+                        if (activity.getCount() == 0) {
+                            showMessage("Aviso", "No hay actividades");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while (activity.moveToNext()) {
+                            buffer.append("Nombre :" + activity.getString(1) + "\n");
+                            buffer.append("Cumplido :" + activity.getString(4) + "\n");
+                        }
+                        showMessage("Estadisticas", buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
     public void ClickButtonDatosPersonales(View view)
